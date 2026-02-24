5e93260c5d67ef55de1ef02255b50f2236d9a126
RadithiaReysa <radithiarp1@gmail.com>
2026-02-25 00:58:05 +0700

    Tugas Pertemuan 3

---
+++ Pertemuan_3/Tugas_3/class_objek.py
@@ -0,0 +1,36 @@
+#Membuat kelas
+class MyClass:
+  x = 5
+print(MyClass)
+
+#membuat objek
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)
+
+#menghapus objek
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+p1 = Person("John", 36)
+del p1
+print(p1) 
+
+#multiple kelas
+class MyClass:
+  x = 5
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+#pass statement kelas
+class Person:
+  pass
